c1b82a3bf5df189e1f609b17f0c9914ea75ac37f
Christoph S. Ackermann <info@acki.be>
2011-12-15 17:10:39 +0100

    Cleaned config

--- _env/config.php
+++ _env/config.php
@@ -5,12 +5,8 @@ $cfg = array(
             'title'=>"workr",
             'shortkey'=>"workr",
             'address'=>"localhost/workr",
-            'defaultlang'=>"de",
             'pagecount'=>"3",
             'pagesteps'=>12,
-            'stockidlength'=>"4",
-            'imgpath'=>"_files/images/original/", // Format: _files/images/original/ (without slashes in front and end of the string)
-            'imgthumbpath'=>"_files/images/thumbs/", // Format: _files/images/thumbs/ (without slashes in front and end of the string)
         ),
         'mysql'=>array(
             'host'=>"localhost",
@@ -21,17 +17,11 @@ $cfg = array(
         'style'=>array(
             'id'=>"workr",
             'path'=>"_style",
-            'expired'=>"darkviolet",
-            'expiresoon'=>"darkred",
-            'expire'=>"red",
         ),
         'auth'=>array(
             'timeout'=>60*60*24*30,
             'utimeout'=>60*60,
             'cookietimeout'=>60*60*24*30,
-        ),
-        'languages'=>array(
-            'de',
         ),
 );
 
