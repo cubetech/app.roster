83f94ccff6eb54a1d6ab611976c41cdca5e88a6d
Christoph S. Ackermann <info@acki.be>
2011-12-16 16:29:02 +0100

    Added some links

--- task/index.php
+++ task/index.php
@@ -3,8 +3,17 @@
 	define('dire', '../');
 	include(dire . '_env/exec.php');
 	
+	$status = vGET('status');
+	
+	if(!$status || $status == '')
+		$status = 1;
+		
+	$sidemsg = '<a href="./?status=1">OFFENE AUFTR&Auml;GE</a>';
+	if($status==1)
+		$sidemsg = '<a href="./?status=2">ABGESCHLOSSENE AUFTR&Auml;GE</a>';
+	
 	$tasks = array();
-	$query = mysql_query('SELECT * FROM `task`') or sqlError(__FILE__,__LINE__,__FUNCTION__);
+	$query = mysql_query('SELECT * FROM `task` WHERE `status`="'.$status.'"') or sqlError(__FILE__,__LINE__,__FUNCTION__);
 	while($fetch=mysql_fetch_array($query))
 		array_push($tasks, $fetch);
 		
@@ -13,15 +22,15 @@
 	while($fetch=mysql_fetch_array($query))
 		$tire[$fetch[0]] = $fetch[1];
 		
-	write_header('Auftr&auml;ge');
+	write_header('Auftr&auml;ge', $sidemsg);
 	
 	?>
-	
+	<br />
 		<table width="850" border="0" cellspacing="0" cellpadding="10" class="table_main">
 		  <tbody><tr style="background-color:#d9d8d8; font-size:14px;">
-			<td width="50"><strong>NR</strong></td>
-			<td width="250"><strong>KUNDE</strong></td>
-			<td width="150"><strong>TYP</strong></td>
+			<td width="40"><strong>NR</strong></td>
+			<td width="330"><strong>KUNDE</strong></td>
+			<td width="80"><strong>TYP</strong></td>
 			<td width="150"><strong>TERMIN</strong></td>
 			<td width="200"><strong>AKTION</strong></td>
 		  </tr>
@@ -37,13 +46,19 @@
 		  		if(strlen($t['company'])>0)
 		  			$t['company'] = $t['company'] . ' | ';
 		  			
+		  		if($t['status'] == 1) {
+		  			$statuschange = '<a href="'.dire.'task/close/?id='.$t['id'].'&status=2">ERLEDIGT</a>';
+		  		} else {
+		  			$statuschange = '<a href="'.dire.'task/close/?id='.$t['id'].'&status=1">ER&Ouml;FFNEN</a>';
+		  		} 
+		  			
 		  		print '
 						<tr class="'.$class.'">
 							<td><a href="'.dire.'task/detail/?id='.$t['id'].'">'.$t['id'].'</a></td>
 							<td><a href="'.dire.'task/detail/?id='.$t['id'].'">'.$t['company'].' '.$t['name'].'</a></td>
 							<td>'.$tire[$t['tire']].'</td>
 							<td>'.date('d.m.Y H:i', $t['duetime']).'</td>
-							<td><a href="#">BEARBEITEN </a>| <a href="#">ERLEDIGT </a></td>
+							<td><a href="'.dire.'task/edit/?id='.$t['id'].'">BEARBEITEN </a>| '.$statuschange.'</td>
 						</tr>
 		  		';
 		  		
