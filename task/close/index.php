243c54e7367d5a5d59ee188a53a457e603098d9f
Christoph S. Ackermann <info@acki.be>
2011-12-16 16:28:15 +0100

    Created close script

---
+++ task/close/index.php
@@ -0,0 +1,32 @@
+<?php
+
+	define('dire', '../../');
+	include(dire . '_env/exec.php');
+	
+	$id = vGET('id');
+	$status = vGET('status');
+	
+	$query = mysql_query('SELECT * FROM `status` WHERE `id`="'.$status.'"') or sqlError(__FILE__,__LINE__,__FUNCTION__);
+	$statuslist = mysql_fetch_array($query);
+	
+	if(!is_array($statuslist)) {
+		_message('Es wurde ein ung&uuml;ltiger Status gesendet. Aktion nicht ausgef&uuml;hrt.', true);
+		header('Location: '.dire.'task/');
+		die();
+	}
+	
+	$query = mysql_query('UPDATE `task` SET `status`="'.$status.'" WHERE `id`="'.$id.'"') or sqlError(__FILE__,__LINE__,__FUNCTION__);
+	
+	$query = mysql_query('SELECT * FROM `task` WHERE `id`="'.$id.'"') or sqlError(__FILE__,__LINE__,__FUNCTION__);
+	$task = mysql_fetch_array($query);
+	
+	$query = mysql_query('SELECT * FROM `users` WHERE `uid`="'.$task['infouser'].'"') or sqlError(__FILE__,__LINE__,__FUNCTION__);
+	$user = mysql_fetch_array($query);
+	
+	sendMail($user['mail'], $user['prename'] . ' ' . $user['name'], 'Auftrag wurde '.$statuslist['text'].'', 'Hallo '.$user['prename'].'\n\nDer Auftrag Nr. '.$task['id'].' wurde '.$statuslist['text'].'.\nDetails: http://'.$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"].'\n\nFreundliche Gr&uuml;sse', 'html');
+	
+	_message('Der Auftrag wurde '.$statuslist['text'].'.');
+	
+	header('Location: '.dire.'task/');
+	
+?>
