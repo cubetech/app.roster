d41df4f1f6d097e372fc50adb2ed87f3cae9ccdf
christoph.ackermann <info@acki.be>
2012-02-02 22:13:29 +0100

    Added database layout

---
+++ roster.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Erstellungszeit: 02. Februar 2012 um 22:12
+-- Server Version: 5.1.46
+-- PHP-Version: 5.3.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Datenbank: `roster`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `auth`
+--
+
+CREATE TABLE IF NOT EXISTS `auth` (
+  `sid` varchar(40) NOT NULL DEFAULT '',
+  `session_type` char(1) NOT NULL DEFAULT '',
+  `user_ip` varchar(15) DEFAULT NULL,
+  `user_hostname` varchar(255) DEFAULT NULL,
+  `lastaction` int(255) DEFAULT NULL,
+  `revisit` int(11) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `clearpwd` varchar(255) DEFAULT NULL,
+  `session` mediumtext NOT NULL,
+  UNIQUE KEY `sid` (`sid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `barcode`
+--
+
+CREATE TABLE IF NOT EXISTS `barcode` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `barcode` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `item`
+--
+
+CREATE TABLE IF NOT EXISTS `item` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `barcode` int(10) NOT NULL COMMENT 'barcode id',
+  `category` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `buydate` int(10) NOT NULL,
+  `buycondition` int(10) NOT NULL,
+  `buylocation` int(10) NOT NULL,
+  `buyprice` float NOT NULL,
+  `buycurrency` int(10) NOT NULL,
+  `comments` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `prename` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `status` enum('false','true') NOT NULL DEFAULT 'false',
+  `lastlogin` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
