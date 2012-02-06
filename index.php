48596790f5a31e8446c3b503c3c6907b425e8c44
Christoph S. Ackermann <info@acki.be>
2012-02-06 17:24:57 +0100

    statistics

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
-<?
+<?php
 
     define('dire', '');
     include(dire . '_env/exec.php');
     
     write_header('Dashboard');
+    
+    ?>
+    
+    	<div class="span3">
+    		<div class="well sidebar-nav">
+    			<ul class="nav nav-list">
+    				<li class="nav-header">Statistik</li>
+    				<li><a href="#">3 Artikel</a></li>
+    				<li><a href="#">37 Kunden</a></li>
+    				<li><a href="#">832 Ausleihpakete</a></li>
+    			</ul>
+    		</div>
+    	</div>
+    	
+    	<div class="span9">
+    	</div>
+    	
+    <?php
     
     write_footer();
     
