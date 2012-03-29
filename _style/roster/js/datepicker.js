e209712070b93cb084855f288d3df1ae63532841
christoph.ackermann <info@acki.be>
2012-03-29 19:05:30 +0200

    Added jQuery and jQuery UI

---
+++ _style/roster/js/datepicker.js
@@ -0,0 +1,25 @@
+jQuery(function($){
+	$.datepicker.regional['de-CH'] = {
+		closeText: 'schliessen',
+		prevText: '&#x3c;zur&uuml;ck',
+		nextText: 'n&auml;chster&#x3e;',
+		currentText: 'heute',
+		monthNames: ['Januar','Februar','M&auml;rz','April','Mai','Juni',
+		'Juli','August','September','Oktober','November','Dezember'],
+		monthNamesShort: ['Jan','Feb','M&auml;r','Apr','Mai','Jun',
+		'Jul','Aug','Sep','Okt','Nov','Dez'],
+		dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+		dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+		dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+		weekHeader: 'Wo',
+		dateFormat: 'dd.mm.yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
+	$.datepicker.setDefaults($.datepicker.regional['de-CH']);
+});    	
+$.datepicker.setDefaults($.datepicker.regional['de-CH']);
+$(function() {
+	$( "#datepicker" ).datepicker();
+});
