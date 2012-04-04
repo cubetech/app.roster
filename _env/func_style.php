1354c14e407e9a761355e44d5a7ef876cfcef5a2
christoph.ackermann <info@acki.be>
2012-04-04 14:39:07 +0200

    Increased the button functionality

--- _env/func_style.php
+++ _env/func_style.php
@@ -12,19 +12,40 @@
         
    } //function 
    
-   function gen_right_btn($right, $righthref, $righticon, $rightbtn) {
-       
-       return '
+    function gen_right_btn($right, $righthref, $righticon, $rightbtn, $alt='', $p=true) {
+        
+        if($alt=='') {
+            $alt = $right;
+        }
+        
+        $btn = '
+                    <a class="' . $rightbtn . '" href="' . $righthref . '" title="' . $alt . '">
+                        <i class="' . $righticon . '"></i> ' . $right . '
+                    </a>
+        ';
+        
+        if($p == true) {
+            return '
                <p class="pull-right">
-                   <a class="' . $rightbtn . '" href="' . $righthref . '">
-                       <i class="' . $righticon . '"></i> ' . $right . '
-                   </a>
+                    ' . $btn . '
                </p>
-       ';
-   
+            ';
+        } else {
+            return $btn;
+        }
+    
+    }
+
+   function addbutton($name='', $href='', $class='btn-danger', $icon='icon-remove icon-white') {
+       if(!@is_array($_SESSION['morebuttons'])) {
+           $_SESSION['morebuttons'] = array();
+       }
+       
+       array_push($_SESSION['morebuttons'], array('name'=>$name, 'href'=>$href, 'class'=>$class, 'icon'=>$icon));
+       
    }
 
-    function linenav($left='', $lefthref='', $right='', $righthref='', $lefticon='icon-chevron-left', $righticon='icon-plus icon-white', $leftbtn='btn', $rightbtn='btn btn-primary') {
+   function linenav($left='', $lefthref='', $right='', $righthref='', $lefticon='icon-chevron-left', $righticon='icon-plus icon-white', $leftbtn='', $rightbtn='btn-primary') {
     
         // Check if session call
         
@@ -44,7 +65,7 @@
                 error('own', 'You don\'t set the lefthref in the function breadnav().');    
             } //if
             
-            $lbtn = gen_left_btn($left, $lefthref, $lefticon, $leftbtn);
+            $lbtn = gen_left_btn($left, $lefthref, $lefticon, 'btn ' . $leftbtn);
             $_SESSION['linenav'] = $lbtn;
             echo $lbtn;
             
@@ -52,14 +73,25 @@
         
         // Right button
         
-        if(isset($right) && $right!='') {
-        
-            if(!isset($righthref)) {
-                error('own', 'You don\'t set the righthref in the function breadnav().');
-            } //if
+        if((isset($right) && $right!='') || @is_array($_SESSION['morebuttons'])) {
             
-            $rbtn = gen_right_btn($right, $righthref, $righticon, $rightbtn);
+            $rbtn = '';
+        
+            if(isset($right) && $right!='') {
+                if(!isset($righthref)) {
+                    error('own', 'You don\'t set the righthref in the function breadnav().');
+                } //if
+                
+                $rbtn .= gen_right_btn($right, $righthref, $righticon, 'btn ' . $rightbtn);
+            }
+            if(@is_array($_SESSION['morebuttons'])) {
+                $_SESSION['morebuttons'] = array_reverse($_SESSION['morebuttons']);
+                foreach($_SESSION['morebuttons'] as $b) {
+                    $rbtn .= gen_right_btn($b['name'], $b['href'], $b['icon'], 'btn ' . $b['class']);
+                }
+            }
             $_SESSION['linenav'] .= $rbtn;
+            $_SESSION['morebuttons'] = array();
             echo $rbtn;
             
         } //if
