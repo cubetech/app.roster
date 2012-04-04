2e448bff4b450baa12d42a5c45283687ab15928a
christoph.ackermann <info@acki.be>
2012-04-04 14:42:06 +0200

    Added buttons and item status

--- item/index.php
+++ item/index.php
@@ -3,14 +3,20 @@
     define('dire', '../');
     include(dire . '_env/exec.php');
     
+    $del = vGET('del');
+
     $query = mysql_query('SELECT i.*, 
                                  c.name AS categoryname,
-                                 b.barcode as fullbarcode
+                                 b.barcode as fullbarcode,
+                                 s.status as statusname
                             FROM item i 
                             LEFT JOIN 
                             category c ON (i.category = c.id)
                             LEFT JOIN
-                            barcode b ON (i.barcode = b.id)') or sqlError(__FILE__,__LINE__,__FUNCTION__);
+                            barcode b ON (i.barcode = b.id)
+                            LEFT JOIN
+                            status s ON (i.status = s.id)
+                            WHERE i.delete!=1') or sqlError(__FILE__,__LINE__,__FUNCTION__);
     $items = array();
     while($fetch=mysql_fetch_array($query))
         array_push($items, $fetch);
@@ -18,6 +24,15 @@
     write_header('Artikelliste');
     
     linenav('Dashboard', dire, 'Neuer Artikel hinzuf&uuml;gen', dire . 'item/new/');
+    
+    if(isset($del) && $del!='') {
+        echo '
+            <div class="alert alert-success">
+              <a class="close" data-dismiss="alert">x</a>
+              <strong>Gel&ouml;scht!</strong> Der Artikel mit der <strong>ID ' . $del . '</strong> wurde gel&ouml;scht.
+              <a href="undo/?id=' . $del . '">R&uuml;ckg&auml;ngig</a>
+            </div>';
+    }
     
     ?>
         
@@ -31,6 +46,7 @@
                     <th>Kategorie</th>
                     <th class="big5">Artikel</th>
                     <th>Status</th>
+                    <th class="tableicons"></th>
                 </tr>
             </thead>
             <tbody>
@@ -45,7 +61,12 @@
                     <td><a href="'.dire.'barcode/detail/?id='.$i['barcode'].'">'.$i['fullbarcode'].'</a></td>
                     <td><a href="'.dire.'category/?id='.$i['category'].'">'.$i['categoryname'].'</a></td>
                     <td><a href="'.dire.'item/detail/?id='.$i['id'].'">'.$i['name'].'</a></td>
-                    <td>here</td>
+                    <td>' . $i['statusname'] . '</td>
+                    <td>' . 
+                    gen_right_btn('', dire . 'barcode/print/?id=' . $i['barcode'], 'icon-print icon-white', 'btn btn-mini btn-inverse', 'Barcode drucken', false) . ' ' . 
+                    gen_right_btn('', 'edit/?id=' . $i['id'], 'icon-pencil icon-white', 'btn btn-mini btn-warning', 'Artikel bearbeiten', false) . ' ' . 
+                    gen_right_btn('', 'delete/?id=' . $i['id'], 'icon-remove icon-white', 'btn btn-mini btn-danger', 'Artikel l&ouml;schen', false) . ' ' . 
+                    gen_right_btn('', dire . 'package/new/?item_id=' . $i['id'], 'icon-fire icon-white', 'btn btn-mini btn-primary', 'Paket erstellen', false) . '</td>
                 </tr>
             ';
             
