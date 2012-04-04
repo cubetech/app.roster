4def52908ebd6a32f691c4d9439eb5e03ad8795f
christoph.ackermann <info@acki.be>
2012-04-04 14:40:36 +0200

    Added target url for the form

--- _style/roster/js/upload.js
+++ _style/roster/js/upload.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
     $('#photoimg').live('change', function() { 
         $("#preview").html('');
         $("#preview").html('<div class="loader">&nbsp;</div>');
-        $("#imageform").ajaxForm({
-            target: '#preview'
+        $("#form").ajaxForm({
+            target: '#preview',
+            url: 'image/'
         }).submit();
     });
 }); 
