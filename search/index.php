5cf71d8026fe5751af2d01c45d4dec816b361e39
christoph.ackermann <info@acki.be>
2012-04-11 10:27:01 +0200

    Fixed and extended search

--- search/index.php
+++ search/index.php
@@ -5,21 +5,24 @@
     
     $search = iGET('search', true, 'string');
     
-    if(strlen($search)!=13) {
-        error('own', 'Dies ist kein g&uuml;ltiger Barcode in diesem System!');
-    }
+    //var_dump($search);
     
-    $query = mysql_query('SELECT * FROM `barcode` WHERE `barcode`="'.$search.'"') or sqlError(__FILE__,__LINE__,__FUNCTION__);
+    if(strlen($search)!=13) {
+        error('own', 'Dies ist kein g&uuml;ltiger Barcode in diesem System! Es wird ein EAN13 Code erwartet.');
+    }
+        
+    $query = mysql_query('SELECT * FROM `barcode` WHERE `barcode` LIKE "'.$search.'%"') or sqlError(__FILE__,__LINE__,__FUNCTION__);
     $barcode = mysql_fetch_array($query);
-
+    
+    //var_dump($barcode);
+    
     $query = mysql_query('SELECT * FROM `item` WHERE `barcode`="'.$barcode['id'].'"') or sqlError(__FILE__,__LINE__,__FUNCTION__);
     $item = mysql_fetch_array($query);
 
-    if(!$barcode) {
-        error('own', 'Dieser Barcode wurde im System nicht gefunden!');
-    }
-    
-    if(!$item) {
+    if($barcode == false) {
+        header('Location: ' . dire . 'item/new/?scode=' . $search);
+        //error('own', 'Dieser Barcode wurde im System nicht gefunden!');
+    } elseif(!$item) {
         header('Location: ' . dire . 'barcode/detail/?id=' . $barcode['id']);
     } else {
         header('Location: ' . dire . 'item/detail/?id=' . $item['id']);
