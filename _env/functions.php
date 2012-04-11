ed659a9ac7951cef39748b96f93be725c3723198
christoph.ackermann <info@acki.be>
2012-04-11 10:26:45 +0200

    Fixed barcode function (checksum thing)

--- _env/functions.php
+++ _env/functions.php
@@ -154,9 +154,11 @@
         }
     }
     
-    function code13() {
+    function code13($code = false) {
     
-        $code = substr(number_format(time() * rand(),0,'',''),0,13);
+        if(!$code) {
+            $code = code13_check(substr(number_format(time() * rand(),0,'',''),0,12));
+        }
         $query = mysql_query('SELECT * FROM barcode WHERE `barcode`="'.$code.'"') OR sqlError(__FILE__,__LINE__,__FUNCTION__);
         $fetch = mysql_fetch_array($query);
         if($fetch)
@@ -165,6 +167,23 @@
             $query = mysql_query('INSERT INTO barcode (barcode, time) VALUES ("'.$code.'", "'.time().'")') OR sqlError(__FILE__,__LINE__,__FUNCTION__);
             return mysql_insert_id();
         
+    }
+    
+    function code13_check($digits){
+        //first change digits to a string so that we can access individual numbers
+        $digits =(string)$digits;
+        // 1. Add the values of the digits in the even-numbered positions: 2, 4, 6, etc.
+        $even_sum = $digits{1} + $digits{3} + $digits{5} + $digits{7} + $digits{9} + $digits{11};
+        // 2. Multiply this result by 3.
+        $even_sum_three = $even_sum * 3;
+        // 3. Add the values of the digits in the odd-numbered positions: 1, 3, 5, etc.
+        $odd_sum = $digits{0} + $digits{2} + $digits{4} + $digits{6} + $digits{8} + $digits{10};
+        // 4. Sum the results of steps 2 and 3.
+        $total_sum = $even_sum_three + $odd_sum;
+        // 5. The check character is the smallest number which, when added to the result in step 4,  produces a multiple of 10.
+        $next_ten = (ceil($total_sum/10))*10;
+        $check_digit = $next_ten - $total_sum;
+        return $digits . $check_digit;
     }
     
     function gen_query($data) {
