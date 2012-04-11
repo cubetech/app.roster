238855a0202a5ccb1482d48239506d36d3949cab
christoph.ackermann <info@acki.be>
2012-04-11 10:47:13 +0200

    Enhanced delete function with undo and set_message

--- item/delete/index.php
+++ item/delete/index.php
@@ -5,8 +5,28 @@
     
     $id = vGET('id');
     
-    mysql_query('UPDATE `item` SET `delete`=1 WHERE `id`=' . $id) or sqlError(__FILE__,__LINE__,__FUNCTION__);
-                                    
-    header('Location: ../?del=' . $id);
+    $query = mysql_query('SELECT * FROM `item` WHERE `id` = "'.$id.'"') or sqlError(__FILE__,__LINE__,__FUNCTION__);
+    $item = mysql_fetch_array($query);
+    
+    if(!$item) {
+        error('transfer');
+    }
+    
+    $delete = 0;
+    
+    if($item['delete']==0) {
+        $delete = 1;
+    }
+    
+    mysql_query('UPDATE `item` SET `delete`="'.$delete.'" WHERE `id`=' . $id) or sqlError(__FILE__,__LINE__,__FUNCTION__);
+    
+    if($delete == 1) {
+        set_message('Gel&ouml;scht!', 'Der Artikel mit der <strong>ID ' . $id . '</strong> wurde gel&ouml;scht.
+        <a href="delete/?id=' . $id . '">R&uuml;ckg&auml;ngig</a>', 'alert-warning');
+        header('Location: ../');
+    } else {
+    set_message('Wiederhergestellt!', 'Der Artikel mit der <strong>ID ' . $id . '</strong> wurde wiederhergestellt.', 'alert-success');
+        header('Location: ../detail/?id=' . $id);
+    }
     
 ?>
