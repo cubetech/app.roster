3fae69cb20ed43873ce2372068534dc5ad138a60
christoph.ackermann <info@acki.be>
2012-04-11 10:27:42 +0200

    Removed unused stuff

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
     define('dire', '');
     include(dire . '_env/exec.php');
     
-    $query = mysql_query('SELECT COUNT(*) FROM `item`') or sqlError(__FILE__,__LINE__,__FUNCTION__);
-    $item = mysql_fetch_row($query);
-    
     $query = mysql_query('SELECT COUNT(*) FROM `item` WHERE `delete`=0') or sqlError(__FILE__,__LINE__,__FUNCTION__);
     $itema = mysql_fetch_row($query);
     
@@ -20,7 +17,7 @@
     		<div class="well sidebar-nav">
     			<ul class="nav nav-list">
     				<li class="nav-header">Statistik</li>
-    				<li><a href="item/"><?=$item[0]?> Artikel (<?=$itema[0]?> aktiv)</a></li>
+    				<li><a href="item/"><?=$itema[0]?> Artikel</a></li>
     				<li><a href="barcode/"><?=$barcode[0]?> Barcodes</a></li>
     				<li><a href="#">0 Kunden</a></li>
     				<li><a href="#">0 Ausleihpakete</a></li>
