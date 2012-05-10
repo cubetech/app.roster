1913ba545570f31497f94ec6568c649ea4da77d0
christoph.ackermann <info@acki.be>
2012-05-10 16:57:42 +0200

    Replaced barcode library and added full size page print

--- barcode/print/index.php
+++ barcode/print/index.php
@@ -3,51 +3,88 @@
     define('dire', '../../');
     include(dire . '_env/exec.php');
     
-    require(dire . '_env/addons/php-barcode/php-barcode.php');
+    require(dire . '_env/addons/Barcode39/Barcode39.php');
     require(dire . '_env/addons/fpdf17/fpdf.php');
     
     $id = vGET('id');
     
-    $query = mysql_query('SELECT * FROM `barcode` WHERE `id`="'.$id.'"') or sqlError(__FILE__,__LINE__,__FUNCTION__);
-    $barcode = mysql_fetch_array($query);
+    if($id) { 
     
-    $pdf = new FPDF($cfg['pdf']['orientation'], 'mm', array($cfg['pdf']['width'], $cfg['pdf']['height']));
+        $query = mysql_query('SELECT * FROM `barcode` WHERE `id`="'.$id.'"') or sqlError(__FILE__,__LINE__,__FUNCTION__);
+        $barcode = mysql_fetch_array($query);
+    
+    }
+    
+    if($barcode) {
+    
+        $pdf = new FPDF($cfg['pdf']['small']['orientation'], 'mm', array($cfg['pdf']['small']['width'], $cfg['pdf']['small']['height']));
+
+    } else {
+            
+        $pdf = new FPDF($cfg['pdf']['big']['orientation'], 'mm', array($cfg['pdf']['big']['width'], $cfg['pdf']['big']['height']));
+    
+    }
 
     $pdf->SetDisplayMode( 50 );
     $pdf->AddPage();
     
-    ob_start();
-    barcode_print($barcode['barcode']);
-    $source = ob_get_contents();
-    ob_end_clean();
+    function createImg($rotate=false, $code=false) {
     
-    $image = $cfg['page']['tmpfolder'] . '/' . time() . rand(100000,999999) . '.png';
+        global $cfg;
+    
+        if(!$code) {
+            $code = code13code();
+        }
+    
+        $image = $cfg['page']['tmpfolder'] . '/' . time() . rand(100000,999999) . '.gif';
+        
+        $bc = new Barcode39($code);
+        $bc->draw($image);
+            
+        if($rotate) {
+            $img = imagecreatefromgif($image);
+            $rotate = imagerotate($img, 90, 0);
+            imagegif($rotate, $image);
+        }
+    
+        $size = getimagesize($image);
+    
+        return array('img'=>$image, 'size'=>$size);
+        
+    }
+    
     $dpi = 100;
-    
-    $resource = imagecreatefromstring($source);
-    $resource = imagerotate($resource, 90, 0);
-    imagepng($resource, $image);
-
-    
-
-    $size = @getimagesize($image);
-    
-
-    // $k = $dpi / 2.54;     // bei cm
-    // $k = $dpi;         // bei inch
-
     $k = $dpi / 25.4; // bei mm
 
     $w = $pdf->w * $k; // get page width pixel
     $h = $pdf->h * $k; // get page height pixel
-
-    $x = (($w/2) - ($size[0]/2)) / $k; // set x-pos page mm
-    $y = (($h/2) - ($size[1]/2)) / $k; // set y-pos page mm
-
-    $pdf->Image($image, $x, $y, $size[0] * 25.4 / $dpi); 
+    
+    if($barcode) {
+    
+        $img = createImg(true, $barcode['barcode']);
+        $x = (($w/2) - ($img['size'][0]/2)) / $k; // set x-pos page mm
+        $y = (($h/2) - ($img['size'][1]/2)) / $k; // set y-pos page mm
+        $pdf->Image($img['img'], $x, 0);
+        
+    } else {
+    
+        for($i=0;$i<=9;($i++)) {
+        
+            $pos = 15+$i*27;
+            
+            $img = createImg();
+            $pdf->Image($img['img'], 31, $pos); 
+            unlink($img['img']);
+            
+            $img = createImg();
+            $pdf->Image($img['img'], 120, $pos); 
+            unlink($img['img']);
+        
+        }
+        
+    }
+    #$pdf->Image($image, $x, $y, $size[0] * 25.4 / $dpi); 
 
     $pdf->Output( 'barcode.pdf', 'I');
-    
-    unlink($image);
 
 ?>
